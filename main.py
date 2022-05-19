98c22c0ffa96e7b4c390949d8a0149554d8f8b75
malen-c <malencuturic@gmail.com>
2022-05-19 16:39:44 -0700

    add conversion to MIDI signal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import copy
+import re
 from random import randint
 
 #duration in seconds
@@ -8,9 +9,19 @@ def sleep(duration):
     while start + duration >= time.perf_counter():
         pass
 
-class note:
-    pitch = 'C'
-    velocity = 0
+#table of natural midi note numbers mod 12
+note_numbers = {'C' : 0, 'D' : 2, 'E' : 4, 'F' : 5, 'G' : 7, 'A' : 9, 'B' : 11}
+num_reg = re.compile('-?\d+')
+def note_to_midi(note):
+    #adds 1 if sharp, subtracts 1 if flat, or both if they included both hehe
+    midi_num = note_numbers[note[0]] + ('#' in note) - ('b' in note)
+    octave_nums = num_reg.search(note)
+    if octave_nums == None:
+        #defaults to 4th octave
+        midi_num += 12 * (4+1)
+    else:
+        midi_num += 12 * (int(octave_nums[0])+1)
+    return(midi_num)
 
 class sequence:
 
@@ -41,30 +52,36 @@ class sequence:
         if tempo <= 0:
             raise Exception('sequence.change_tempo(new_tempo) : tempo must be positive int or float')
         else:
-            self.tempo = new_tempo
+            self._tempo = new_tempo
             self._step_length = 60/self._tempo*0.25
+    
+    def add_note(self, step, pitch, velocity = 127):
+        #user input steps range from 1 to 16 even though sequence is 0 to 15
+        self._pattern[step-1][note_to_midi(pitch)] = velocity
 
-    def play_note(self, pitch='C', velocity=127):
+    def play_note(self, pitch=60, velocity=127):
         print(('bop!', 'boop!', 'bum,',
                'tiss!', 'bap!', 'tskkk!',
                'WAP!')[randint(0, 6)], end = ' ')
+        print(pitch, velocity)
 
     def play(self):
         playing = True
-        current_step = 1
-        end = self._length * 4
+        #absolute step can exceed length of loop, e.g. you can be on step 18 of a 16 step loop if you're on the second repeat
+        abs_step = 1
+        end = self._length
         while playing:
-            for note in self._pattern[current_step - 1]:
-                self.play_note(self._pattern[current_step - 1][note].pitch,
-                               self._pattern[current_step - 1][note].velocity)
+            #seq step cannot exceed length of loop
+            seq_step = (abs_step - 1) % self._length
+            for note in self._pattern[seq_step]:
+                #current step mod length of pattern so it repeats
+                self.play_note(note, self._pattern[seq_step][note])
             sleep(self._step_length)
-            if current_step == end:
+            if abs_step == end:
                 playing = False
-            current_step += 1
-            
-                
-            
-            
+            abs_step += 1
 
 
 x = sequence()
+for i in range(1,16):
+    x.add_note(i, 'C5')
