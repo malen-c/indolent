66dd8b1cc9dc556c65c3e48ca4edea1700486183
Malen Cuturic <76707891+malen-c@users.noreply.github.com>
2023-10-06 21:36:03 -0700

    Set up templates, style

--- app/app.py
+++ app/app.py
@@ -1,11 +1,12 @@
-import os
-
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from indolent import *
 
 app = Flask(__name__)
 app.debug = True
+app.secret_key = 'bla bla bla'
 
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    flash('test')
+    return render_template('index.html')
